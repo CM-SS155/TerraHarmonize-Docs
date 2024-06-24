a082eeca4df96c232bac4cdd246bab10e6e76447
chandramohan <chandramohan@satsure.co>
2024-06-24 16:07:56 +0530

    Updated the html

---
+++ TerraHarmonize.rst
@@ -0,0 +1,11 @@
+TerraHarmonize module
+=====================
+
+.. automodule:: TerraHarmonize
+   :members:
+   :show-inheritance:
+   :exclude-members: change_char_match, clean_bandit, pattern_od, pattern_kan, pattern_tel, pattern_hi, pattern_eng, replace_dict_Od, replace_dict_Hin, replace_dict_kar, replace_dict_tel
+   :undoc-members:
+
+
+
---
+++ conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import sphinx_pdj_theme
+
+sys.path.insert(0, os.path.abspath('E:\Satsure\SAGE\codes\PYTHON\codes-part2\Packaging_class\src\TerraHarmonize'))
+
+def setup(app):
+    app.add_css_file('my_style.css')
+
+project = 'TerraHarmonize'
+copyright = '2024, chandramohan'
+author = 'chandramohan'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    ]
+
+autodoc_typehints = 'none'
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+# html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_static_path = ['_static']
+html_show_sourcelink = False
