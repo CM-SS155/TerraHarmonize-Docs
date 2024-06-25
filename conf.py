694e7b3a87df48119a966b40d44bea6251d0094d
CM-SS155 <167742162+CM-SS155@users.noreply.github.com>
2024-06-25 14:24:22 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -45,5 +45,5 @@ if os.getenv('READTHEDOCS'):
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 # html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
-# html_static_path = ['_static']
+html_static_path = ['_static']
 html_show_sourcelink = False
